16de36ab80fef2731b5daff5bddab0191ca2cabb
SimonAydin <71080343+SimonAydin@users.noreply.github.com>
2020-09-14 18:53:45 +0200

    Add files via upload

---
+++ Okviri/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Okviri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] s = Console.ReadLine().ToCharArray();
+            string[] rows = new string[5];
+            int counter = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i == 0)
+                {
+                    rows[0] += "..#..";
+                    rows[1] += ".#.#.";
+                    rows[2] += "#." + s[i] + ".#";
+                    rows[3] += ".#.#.";
+                    rows[4] += "..#..";
+                    counter++;
+                }
+                else if (counter == 0 && i != 0)
+                {
+                    rows[0] += ".#..";
+                    rows[1] += "#.#.";
+                    rows[2] += "." + s[i] + ".#";
+                    rows[3] += "#.#.";
+                    rows[4] += ".#..";
+                    counter++;
+                }
+                else if (counter == 1 && i != s.Length - 1)
+                {
+                    rows[0] += ".#.";
+                    rows[1] += "#.#";
+                    rows[2] += "." + s[i] + ".";
+                    rows[3] += "#.#";
+                    rows[4] += ".#.";
+                    counter++;
+                }
+                else if (counter == 1 && i == s.Length - 1)
+                {
+                    rows[0] += ".#..";
+                    rows[1] += "#.#.";
+                    rows[2] += "." + s[i] + ".#";
+                    rows[3] += "#.#.";
+                    rows[4] += ".#..";
+                    counter++;
+                }
+                else if (counter == 2)
+                {
+                    rows[0] += "..*..";
+                    rows[1] += ".*.*.";
+                    rows[2] += "*." + s[i] + ".*";
+                    rows[3] += ".*.*.";
+                    rows[4] += "..*..";
+                    counter = 0;
+                }
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                Console.WriteLine(rows[j]);
+            }
+        }
+    }
+}
